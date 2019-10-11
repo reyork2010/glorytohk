961d2ca9efae4f354e9fc2d652ff3a86e9953d8a
reyork2010 <31087677+reyork2010@users.noreply.github.com>
2019-10-11 23:04:03 +0800

    made by Dad

---
+++ May Glory to Hong Kong.rb
@@ -0,0 +1,222 @@
+
+# 🇭🇰  Wish the Glory Belongs to Hong Kong 🇭🇰
+use_bpm 100
+use_synth :piano
+
+# 何以這土地淚再流
+
+play :G3, attack: 0.05, break: 0.5
+sleep 0.5
+play :C, attack: 0.05, break: 0.5
+sleep 1
+
+play :C, attack: 0.05, break: 0.5
+sleep 0.5
+play :D, attack: 0.05, break: 0.5
+# play :E3
+sleep 0.5
+
+play :B3, attack: 0.05, break: 0.5
+sleep 1
+
+play :B3, attack: 0.05, break: 0.5
+sleep 0.5
+play :C, attack: 0.05, break: 0.5
+sleep 0.5
+
+play :A3, attack: 0.05, sustain: 0.25, break: 2.5
+sleep 2.5
+
+# 何以令衆人亦憤恨
+
+play :C
+sleep 0.5
+play :F
+sleep 1
+play :E
+sleep 0.5
+play :F
+sleep 0.5
+play :D
+sleep 1
+play :E
+sleep 0.5
+play :F
+sleep 0.5
+
+play :E, attack: 0.05, sustain: 0.25, break: 2
+sleep 1
+
+# 昂首拒默沉
+
+sleep 1
+play :C
+sleep 0.5
+play :A4
+sleep 1
+play :F
+sleep 0.5
+play :E
+sleep 0.5
+play :D
+sleep 1
+
+# 吶喊聲響透
+
+play :E
+sleep 0.5
+play :F
+sleep 0.5
+play :G
+sleep 1
+play :E
+sleep 1
+play :C
+sleep 1
+
+# 盼自由歸於這裏
+
+play :C
+sleep 0.5
+play :B3
+sleep 0.5
+play :A3
+sleep 1
+play :F
+sleep 0.5
+play :E
+sleep 1
+
+play :D, attack: 0.05, break: 0.5
+sleep 0.5
+play :C, attack: 0.05, break: 0.5
+sleep 2
+
+## 何以這恐懼抹不走
+
+play :G3, attack: 0.05, break: 0.5
+sleep 0.5
+play :C, attack: 0.05, break: 0.5
+sleep 1
+
+play :C, attack: 0.05, break: 0.5
+sleep 0.5
+play :D, attack: 0.05, break: 0.5
+# play :E3
+sleep 0.5
+
+play :B3, attack: 0.05, break: 0.5
+sleep 1
+
+play :B3, attack: 0.05, break: 0.5
+sleep 0.5
+play :C, attack: 0.05, break: 0.5
+sleep 0.5
+
+play :A3, attack: 0.05, sustain: 0.5, break: 2.5
+sleep 2.5
+
+# 何以爲信念沒退後
+
+play :C
+sleep 0.5
+play :F
+sleep 1
+play :E
+sleep 0.5
+play :F
+sleep 0.5
+play :D
+sleep 1
+play :E
+sleep 0.5
+play :F
+sleep 0.5
+
+play :E, attack: 0.05, sustain: 0.25, break: 0.5
+sleep 1
+
+
+# 何解血在流
+
+play :C
+sleep 1
+play :A
+sleep 1
+play :F
+sleep 0.5
+play :E
+sleep 0.5
+play :D
+sleep 0.5
+
+# 但邁進聲響透
+play :D
+sleep 0.5
+play :E
+sleep 0.5
+play :F
+sleep 0.5
+play :G
+sleep 0.5
+sleep 0.5
+play :E
+sleep 0.5
+sleep 0.5
+play :C
+sleep 1
+
+# 建自由光輝香港
+
+play :C
+sleep 0.5
+play :B3
+sleep 0.5
+
+#19
+play :A3
+sleep 1
+play :F
+sleep 0.5
+play :E
+sleep 1
+play :D
+sleep 0.5
+play :C, sustain: 1.5, break: 0.5
+sleep 2
+
+# 在晚星墜落 彷徨午夜
+
+play :B3
+sleep 0.5
+play :C
+sleep 1
+play :D
+sleep 1.5
+play :B3
+sleep 0.5
+play :B3
+sleep 0.5
+
+play :A3
+sleep 0.5
+play :A3
+sleep 0.5
+play :F
+sleep 0.5
+play :E
+sleep 2
+
+# 迷霧裏最遠處吹來號角聲
+play :D
+sleep 1
+play :E
+sleep 1
+play :F
+sleep 1.5
+play :F
+sleep 0.5
+play :F
+sleep 0.5
+play :F
+sleep 1.5
